f3782373a04efb227f765dad9a9978ca8ca1dfac
edu-ti <educs85.ti@gmail.com>
2024-10-10 20:49:36 -0300

    Atualização do condigo e finalização do desafio

--- index.js
+++ index.js
@@ -3,5 +3,28 @@ function calcularRank(vitorias, derrotas) {
     // Calculando o saldo de vitórias
     const saldoVitorias = vitorias - derrotas;
     let nivel = "";
+    // Definindo o nível baseado no número de vitórias
+    if (saldoVitorias <= 10) {
+        nivel = "Ferro";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else if (saldoVitorias >= 101) {
+        nivel = "Imortal";
+    }
+    // Exibindo a mensagem final
+    console.log(`O Herói tem saldo de ${saldoVitorias} e está no nível de ${nivel}.`);
+    //console.log("O Herói tem de saldo de " + saldoVitorias + " e está no nível de " + nivel+".")
 }
-   
+// Exemplo de uso
+const vitorias = 250; // Número de vitórias do jogador
+const derrotas = 223; // Número de derrotas do jogador
+
+calcularRank(vitorias, derrotas); 
