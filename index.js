47dc48084dbbc9515a0aedb5eed9dd8e57a4f744
edu-ti <educs85.ti@gmail.com>
2024-10-10 20:45:24 -0300

    Iniciando o Desafio

---
+++ index.js
@@ -0,0 +1,7 @@
+//Desafio Calculadora de Partidas Rankeadas
+function calcularRank(vitorias, derrotas) {
+    // Calculando o saldo de vitórias
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+}
+   
